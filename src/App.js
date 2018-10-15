b2f8e6a35a55e292328311020bb6c8da8e6b5391
renwenhao <1097481091@qq.com>
2018-10-16 00:05:16 +0800

    react-redux

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import {connect} from 'react-redux'
+import { addGun ,removeGun,addGunAsync} from './reducer'
+class App extends Component {
+  render() {
+    console.log('this.props:',this.props);
+    const {state,addGun,removeGun,addGunAsync} = this.props;
+    return (
+      <div>
+        <h1>现在有机枪{state}把</h1>
+        <button onClick = {addGun}>申请武器</button>
+        <button onClick = {removeGun}>上交武器</button>
+        <button onClick = {addGunAsync}>两秒后拿武器</button>
+      </div>
+      
+    );
+  }
+}
+App = connect(state=>({state}),{addGun ,removeGun,addGunAsync})(App);
+export default App;
---
+++ src/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { createStore,compose,applyMiddleware } from 'redux';
+import thunk from 'redux-thunk'
+import { Provider } from 'react-redux' 
+import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
+import {counter} from './reducer'
+const store = createStore(counter,compose(
+    applyMiddleware(thunk),
+    window.devToolsExtension ?  window.devToolsExtension() : f => f
+));
+ReactDOM.render((<Provider store = {store}>
+    <App />
+</Provider>), document.getElementById('root'));
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: http://bit.ly/CRA-PWA
+serviceWorker.unregister();
